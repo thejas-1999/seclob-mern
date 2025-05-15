4e2fdc4e26899288c5937193981c40b8a64af89b
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-05-15 15:07:36 +0530

    setup the create product

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
+import productRouter from "./routes/productRoutes.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8000;
 const mongoUri = process.env.MONGO_URI;
@@ -12,6 +15,7 @@ app.get("/", (req, res) => {
   res.send(`Hello world`);
 });
 
+app.use("/api/products", productRouter);
 mongoose.connect(mongoUri).then(() => {
   console.log(`database is connected to mongodb`);
   app.listen(port, () => {
